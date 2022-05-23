d3322a987c5de7625c40b0b74a6936bdd47e6758
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-23 09:57:02 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './Pages/Login/Login';
 import SignUp from './Pages/Login/SignUp';
 import Home from './Pages/Home/Home';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div  className='max-w-7xl mx-auto'>
+      <Navbar></Navbar>
      <Routes>
        <Route path='/' element={<Home></Home>}></Route>
        <Route path='/login' element={<Login></Login>}></Route>
